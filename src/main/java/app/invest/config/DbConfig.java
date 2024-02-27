f96b72cb1139922ae0e4fcf187c4868338222ecc
emanuelluz <emanuel.luz@bol.com.br>
2024-02-27 13:42:55 -0300

    "crud= listar todos  e criar user v1 + autonomus database"

--- src/main/java/app/invest/config/DbConfig.java
+++ src/main/java/app/invest/config/DbConfig.java
@@ -1,28 +1,28 @@
 package app.invest.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import javax.sql.DataSource;
+//import org.springframework.core.env.Environment;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//import javax.sql.DataSource;
 
 @Configuration
 public class DbConfig {
 
-    @Autowired
-    private Environment env;
+   // @Autowired
+    //private Environment env//;
 
-    @Bean
-    public DataSource dataSource() {
+    //@Bean
+   /* public DataSource dataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.sqlite.JDBC");
         dataSource.setUrl("jdbc:sqlite:demo.db");
         dataSource.setUsername(env.getProperty("sa"));
         dataSource.setPassword(env.getProperty("sa"));
         return dataSource;
-    }
+    }*/
 
 
 }
